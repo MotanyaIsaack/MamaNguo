709f8733b590b64da0d6f11397548dc6f6c19487
Isaack Motanya <isaackmotanya6@gmail.com>
2019-07-08 00:57:18 +0300

    Revert "Fix bugs"

--- src/main/java/com/example/mamanguovendor/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.mamanguovendor.util;
-
-public class Constants {
-    public static final String BASE_URL = "https://adcare.sb.ke/mamanguo/api/";
-//    public static final String TEST_URL = "http://dd066b9e.ngrok.io/api/";
-}
